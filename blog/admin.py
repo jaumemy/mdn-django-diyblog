dd805f5d75bbce7cf34f94bf0b4165949fb25f4b
jaumemy <jaume.montane@outlook.com>
2021-02-25 18:20:02 -0500

    Part 3 - List of all blogs UVT

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Blog, BlogAuthor, BlogComment
+
+
 
 # Register your models here.
+
+
+
+admin.site.register(Blog)
+admin.site.register(BlogAuthor)
+admin.site.register(BlogComment)
--- blog/models.py
+++ blog/models.py
@@ -1,26 +1,10 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
-import uuid
 from django.contrib.auth.models import User
 
 
 # Create your models here.
-
-
-class Blog(models.Model):
-
-    name = models.CharField(max_length=200, help_text='Enter the name/title of the blog')
-    description = models.TextField(max_length=2000, help_text='Write description/content of the blog here')
-    author = models.ForeignKey('BlogAuthor', on_delete=models.SET_NULL, null=True)
-    post_date = models.DateField(default=date.today)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('blog-detail', args=[str(self.id)])
-
 
 
 class BlogAuthor(models.Model):
@@ -29,10 +13,30 @@ class BlogAuthor(models.Model):
     bio = models.TextField(max_length=1000, help_text='Write a brief bio about the author')
 
     def __str__(self):
-        return self.name
+        return self.name.username
 
     def get_absolute_url(self):
         return reverse('blogauthor-detail', args=[str(self.id)])
+
+
+
+
+class Blog(models.Model):
+
+    name = models.CharField(max_length=200, help_text='Enter the name/title of the blog')
+    description = models.TextField(max_length=2000, help_text='Write description/content of the blog here')
+    author = models.ForeignKey(BlogAuthor, on_delete=models.SET_NULL, null=True)
+    post_date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('blog-detail', args=[str(self.id)])
+
+    class Meta:
+        ordering = ['-post_date']
+
 
 
 
@@ -40,12 +44,12 @@ class BlogComment(models.Model):
 
     description = models.TextField(max_length=1000, help_text='Write your comment here')
     post_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True),
-    blog = models.ForeignKey(Blog, on_delete=models.SET_NULL, null=True),
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    blog = models.ForeignKey(Blog, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
 
-        max_len = 75
+        max_len = 25
 
         if len(self.description) > max_len:
             short_description = self.description[:max_len] + '...'
